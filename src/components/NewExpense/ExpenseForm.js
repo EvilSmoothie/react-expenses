5f8c0b922acdcae11820694a67d5ecb51360bd40
EvilSmoothie <8687234+EvilSmoothie@users.noreply.github.com>
2023-04-05 10:04:10 +0100

    added script to preventdefault form submission and create object with new entry

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+//   **ALTERANTIVE**
+// const [userInput, SetUserInput] = useState({
+//     enteredtitle: '',
+//     enteredAmount: '',
+//     enteredDate: ''
+// })
 
   const titleChangeHandler = (e) => {
     setEnteredTitle(e.target.value);
+    // **ALT**
+    // SetUserInput((prevState) => {
+    //     return {...prevState, enteredtitle: e.target.value};
+    // })
   };
 
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
+    // **ALT**
+    // SetUserInput((prevState) => {
+    //     return {...prevState, enteredAmount: e.target.value};
+    // })
   };
 
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
+    // **ALT**
+    // SetUserInput((prevState)=> {
+    //     return {...prevState, enteredAmount: e.target.value}
+    // })
   };
 
+const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
+
+}
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__cojntrol">
           <label>Title</label>
@@ -28,12 +61,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__cojntrol">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__cojntrol">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
-
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
